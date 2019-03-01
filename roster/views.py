00998b4b8c7aa25211b371b4eac0975eae2a273e
Emil Maruszczak <emilekm@gmail.com>
2019-03-01 01:39:31 +0100

    Adjust the visual styling and group players by rank

--- roster/views.py
+++ roster/views.py
@@ -1,3 +1,5 @@
+from itertools import groupby
+
 from django.views.generic.list import ListView
 from django.shortcuts import render
 
@@ -6,4 +8,16 @@ from roster.models import Squad
 
 class SquadListView(ListView):
     model = Squad
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['squads'] = [
+            (squad, [
+                (rank, list(roles))
+                for rank, roles in groupby(squad.players.through.objects.filter(squad=squad), lambda x: x.get_role_display())
+            ])
+            for squad in self.object_list
+        ]
+        return context
+
 
