0f4bd029938f2236199d5059c28fc5773ea5a003
Emil Maruszczak <emilekm@gmail.com>
2019-12-23 01:22:28 +0100

    Update setup with requirements

--- src/setup.py
+++ src/setup.py
@@ -5,6 +5,11 @@ setuptools.setup(
     name='contact-roster',
     packages=setuptools.find_packages(),
     install_requires=[
-        'django'
+        "gunicorn",
+        "django~=2.2.2",
+        "mysqlclient<1.4",
+        "django-guardian",
+        "phpserialize",
+        "bcrypt",
     ]
 )
